46a15e657caef16c032a999f881d8211778a897d
elyoryusupov57-cyber <elyoryusupov57@gmail.com>
2026-04-30 15:11:35 +0300

    Add files via upload
    Odd or even\

---
+++ odd or even.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int a;
+	cout << "Input a:";
+	cin >> a;
+	
+	if (a%2)
+	{
+		cout << "odd" << endl;
+	}
+	else
+	{
+		cout << "even" << endl;
+	}
+	
+	return 0;
+}
