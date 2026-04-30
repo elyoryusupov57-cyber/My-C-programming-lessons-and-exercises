9c0c28e42ef3f9ebc4fbbafada5f9a81467047f0
elyoryusupov57-cyber <elyoryusupov57@gmail.com>
2026-04-30 15:08:58 +0300

    Add files via upload

---
+++ Last digetd of the n number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x;
+	cout << "Input x:" << endl;
+	cin >> x;
+	
+	int y=x-1;
+	cout << "Last digit of natural number:" << y << endl;
+	
+	system("pause");
+	return 0;
+}
