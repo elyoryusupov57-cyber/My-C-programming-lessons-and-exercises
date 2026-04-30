3ede2d211cca779a059f57d4bf584faa932c80d6
elyoryusupov57-cyber <elyoryusupov57@gmail.com>
2026-04-30 15:16:00 +0300

    Add files via upload
    random picking numbers

---
+++ random picking of numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Choose number  between (1-10)"<< endl;
+	int number;
+	if(number>5)
+		cout << "You won the game!!! ";
+	else
+		cout << "oohh my bad you lost game!!";
+	
+	return 0;
+}
