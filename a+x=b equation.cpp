40df0534bbd70431a0c54158867544b1a332a40a
elyoryusupov57-cyber <elyoryusupov57@gmail.com>
2026-04-30 13:11:26 +0300

    Add files via upload

---
+++ a+x=b equation.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double a, b;
+	
+	cout << "Input a: ";
+	cin >> a;
+	
+	cout << "Input b: ";
+	cin >> b;
+	
+	double x = b-a;
+	
+	cout << "Root of equation a+x=b is "
+<< x << endl;
+
+    system ("pause");
+    return 0;
+}
