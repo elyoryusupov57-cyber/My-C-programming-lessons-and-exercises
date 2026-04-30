41518491933e57c5b79339c351c70da9fc4a5632
elyoryusupov57-cyber <elyoryusupov57@gmail.com>
2026-04-30 14:59:01 +0300

    Add files via upload
    This is bit more weird isn't

---
+++ Good morning name and age.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	string a;
+	cout << " What is your name? ";
+	cin >> a;
+	int b;
+	cout << "What is your age? ";
+	cin >> b;
+	
+	cout << "Good Morning " << a << " who is " << b << " years old.";
+	
+	return 0;
+}
