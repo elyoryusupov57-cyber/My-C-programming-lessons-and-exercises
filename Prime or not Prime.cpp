c5312b57d37f013be7dec6ff53bd3e9fc5cee268
elyoryusupov57-cyber <elyoryusupov57@gmail.com>
2026-04-30 15:14:48 +0300

    Add files via upload
    prime or not

---
+++ Prime or not Prime.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int f, c;
+	cout << "Input f:" << endl;
+	cin >> f;
+	
+	if(f>1)
+	{
+
+		cout << " prime:" << c << endl;
+	}
+	else
+	{
+		cout << " not Prime:" << c << endl;
+	}
+	
+	return 0;
+}
